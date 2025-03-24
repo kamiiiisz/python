8576cb058a5854391c0ef916a2021b252431958a
kamiiiisz <kamilly.santos19@escola.pr.gov.br>
2025-03-24 08:48:15 -0300

    atualizaçao codigo em python

--- python.py
+++ python.py
@@ -1,1 +1,8 @@
+idade = int(input('qual sua idade?'))
 
+if(idade<16):
+  print('nao precisa votar!')
+elif(idade == 16 or idade > 65) :
+    print('voto nao obrigatorio')
+else:
+    print('voto obrigatorio')
